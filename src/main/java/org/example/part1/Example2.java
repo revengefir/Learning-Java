ffa39f6162efa67e147810bcf4464c9d2fbe30d4
RevengeFir <vladimirvagero@yandex.ru>
2024-05-26 21:09:52 +0300

    New Code

---
+++ src/main/java/org/example/part1/Example2.java
@@ -0,0 +1,13 @@
+package org.example.part1;
+
+public class Example2 {
+    public static void main(String[] args){
+        int myVar1;
+        int myVar2;
+        myVar1 = 1024;
+        System.out.println("myVar1 содержит " + myVar1);
+        myVar2 = myVar1 / 2;
+        System.out.println("myVar2 содержит myVar1 / 2:" );
+        System.out.println(myVar2);
+    }
+}
