12d3c2ee653c2c9625208576ca86f62cdc900d52
RevengeFir <vladimirvagero@yandex.ru>
2024-05-27 16:06:16 +0300

    New Code

---
+++ src/main/java/org/example/part1/ForDemo.java
@@ -0,0 +1,11 @@
+package org.example.part1;
+
+public class ForDemo {
+    public static void main(String[] args){
+        int count;
+        for(count = 0; count < 5; count++)
+            System.out.println("Результат " + count);
+
+        System.out.println("Готово!");
+    }
+}
