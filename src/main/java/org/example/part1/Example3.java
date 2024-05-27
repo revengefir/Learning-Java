0e6fa90ebe6971584d504a3dc72ac76c5387e59e
RevengeFir <vladimirvagero@yandex.ru>
2024-05-27 10:20:11 +0300

    New Code

---
+++ src/main/java/org/example/part1/Example3.java
@@ -0,0 +1,17 @@
+package org.example.part1;
+
+public class Example3 {
+    public static void main(String[] args){
+        int v;
+        double x;
+        v = 10;
+        x = 10.0;
+        System.out.println("v первоначально " + v);
+        System.out.println("x первоначально " + x);
+        System.out.println();
+        v = v / 4;
+        x = x / 4;
+        System.out.println("v после деления " + v);
+        System.out.println("x после деления " + x);
+    }
+}
---
+++ src/main/java/org/example/part1/exercises/Exercise1GalToLit.java
@@ -0,0 +1,12 @@
+package org.example.part1.exercises;
+
+public class Exercise1GalToLit {
+    public static void main(String[] args){
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+        System.out.println(gallons + " галлонов в литрах равно " + liters);
+    }
+}
