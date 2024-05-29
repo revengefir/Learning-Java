2de5e3d64b42cfc2528d3ec48ed995acf8fe2c40
RevengeFir <vladimirvagero@yandex.ru>
2024-05-29 13:33:27 +0300

    New Code

---
+++ src/main/java/org/example/part1/BlockDemo.java
@@ -0,0 +1,14 @@
+package org.example.part1;
+
+public class BlockDemo {
+    public static void main(String[] args){
+        double i, j, d;
+        i = 5;
+        j = 10;
+        if (i != 0) {
+            System.out.println("i не равно нулю");
+            d = j / i;
+            System.out.println("Результат j / i равняется " + d);
+        }
+    }
+}
