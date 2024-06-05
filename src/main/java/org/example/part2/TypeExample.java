c8497778b81f0cdb9d3ef5b1b3f11feb6c9fe153
RevengeFir <vladimirvagero@yandex.ru>
2024-06-05 13:49:10 +0300

    New Code

---
+++ src/main/java/org/example/part2/TypeExample.java
@@ -0,0 +1,12 @@
+package org.example.part2;
+
+public class TypeExample {
+    public static void main(String[] args) {
+        long ci;
+        long im;
+        im = 5280 * 12;
+        ci = im * im * im;
+        System.out.println("В кубической миле содержится " + ci +
+                " кубических дюймов.");
+    }
+}
